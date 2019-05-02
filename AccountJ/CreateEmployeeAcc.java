700954b91a0f6ed1faf23c3dcb15218eba338f33
Danushka <dhanushkanuwan24@gmail.com>
2019-05-02 15:33:19 +0530

    Add Employee completed

--- AccountJ/CreateEmployeeAcc.java
+++ AccountJ/CreateEmployeeAcc.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class CreateCustomerACC implements iCreateAccout{
+public class CreateEmployeeACC implements iCreateAccout{
 
     private String userID;
     private String name;
